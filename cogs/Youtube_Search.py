0ff41bfff8bdf996b74f59a3c296e6289f9d24ee
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-17 16:36:16 +0300

    Update Youtube_Search.py

--- cogs/Youtube_Search.py
+++ cogs/Youtube_Search.py
@@ -10,10 +10,12 @@ class Youtube_Search(commands.Cog):
 	@commands.command()
 	async def youtube(self, ctx, *, search):
 
-		query_string = urllib.parse.urlencode({'ytsearch': search})
+		query_string = urllib.parse.urlencode({
+			'search_query': search
+		})
 		htm_content = urllib.request.urlopen('http://www.youtube.com/results?' + query_string)
-		search_results = re.findall('href=\"\\/watch\\?v=(.{11})', htm_content.read().decode())
-		await ctx.send('http://www.youtube.com/watch?v=' + search_results[0])
+		self.search_results = re.findall('href=\"\\/watch\\?v=(.{11})', htm_content.read().decode())
+		await ctx.send('http://www.youtube.com/watch?v=' + self.search_results[0])
 
 	@commands.Cog.listener()
 	@youtube.error
