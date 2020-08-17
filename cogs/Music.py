b86160feaef0249b8a8f864f53f3977707bdc9f0
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-17 03:55:57 +0300

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -210,7 +210,7 @@ class Music(commands.Cog):
 				add_queue = False
 				self.queues[q_num] = q_num
 
-		queue_path = os.path.abspath(os.path.realpath("Queue") + f"\song{q_num}.%(ext)s")
+		queue_path = os.path.abspath(os.path.realpath("Queue") + f"/song{q_num}.%(ext)s")
 
 		ydl_opts = {
 			'format': 'bestaudio/best',
