dd65f43e66eb681787181e9dc3e5b65dc989fab2
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-18 23:24:38 +0300

    Update Remind.py

--- cogs/Remind.py
+++ cogs/Remind.py
@@ -4,24 +4,24 @@ import asyncio
 from discord.ext import commands, timers
 
 class Remind(commands.Cog):
-   def __init__(self, bot):
-      self.bot = bot
-      self.bot.timer_manager = timers.TimerManager(self.bot)
-        
-  @bot.command()
-  async def remind(ctx, time, *, text):
-      msg_date = ctx.message.created_at
-      date = msg_date + datetime.timedelta(hours = float(time))
-      mf = msg_date + datetime.timedelta(hours = 3)  #костыли из-за часовых поясов
-      md = date + datetime.timedelta(hours = 3)      #костыли из-за часовых поясов
-      await ctx.channel.send("Принято: сообщение \"{}\" будет отправлено {}.".format(text, md.strftime("%d.%m.%Y в %H:%M (МСК)")))
-      self.bot.timer_manager.create_timer("reminder", date, args=(ctx.channel.id, mf.strftime("%d.%m.%Y %H:%M (МСК)"), ctx.author, text))
+	def __init__(self, bot):
+	self.bot = bot
+	self.bot.timer_manager = timers.TimerManager(self.bot)
 
-    
-  @bot.event
-  async def on_reminder(channel_id, date, author, text):
-      channel = self.bot.get_channel(channel_id)
-      await channel.send("{}, твоё напоминание от {} : {}".format(author.mention, date, text))
+	@commands.command()
+	async def remind(self, ctx, time, *, text):
+		msg_date = ctx.message.created_at
+		date = msg_date + datetime.timedelta(hours = float(time))
+		mf = msg_date + datetime.timedelta(hours = 3)  #костыли из-за часовых поясов
+		md = date + datetime.timedelta(hours = 3)      #костыли из-за часовых поясов
+		await ctx.channel.send("Принято: сообщение \"{}\" будет отправлено {}.".format(text, md.strftime("%d.%m.%Y в %H:%M (МСК)")))
+		self.bot.timer_manager.create_timer("reminder", date, args=(ctx.channel.id, mf.strftime("%d.%m.%Y %H:%M (МСК)"), ctx.author, text))
+
+
+	@commands.Cog.listener()
+	async def on_reminder(self, channel_id, date, author, text):
+		channel = self.bot.get_channel(channel_id)
+		await channel.send("{}, твоё напоминание от {} : {}".format(author.mention, date, text))
 
 
 def setup(bot):
