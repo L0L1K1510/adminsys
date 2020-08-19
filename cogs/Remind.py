26cc95020f87dbd75f016c634d4951e4b1b64c89
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-19 22:59:02 +0300

    Update Remind.py

--- cogs/Remind.py
+++ cogs/Remind.py
@@ -3,19 +3,27 @@ import datetime
 import asyncio
 from discord.ext import commands, timers
 
+
 class Remind(commands.Cog):
 	def __init__(self, bot):
 	self.bot = bot
 	self.bot.timer_manager = timers.TimerManager(self.bot)
+	
+	UTC_time = 3
+	
+	@commands.command()
+	async def remind(self, ctx, utime):
+		UTC_time = utime
+		return UTC_time
 
 	@commands.command()
 	async def remind(self, ctx, time, *, text):
 		msg_date = ctx.message.created_at
 		date = msg_date + datetime.timedelta(hours = float(time))
-		mf = msg_date + datetime.timedelta(hours = 3)  #костыли из-за часовых поясов
-		md = date + datetime.timedelta(hours = 3)      #костыли из-за часовых поясов
-		await ctx.channel.send("Принято: сообщение \"{}\" будет отправлено {}.".format(text, md.strftime("%d.%m.%Y в %H:%M (МСК)")))
-		self.bot.timer_manager.create_timer("reminder", date, args=(ctx.channel.id, mf.strftime("%d.%m.%Y %H:%M (МСК)"), ctx.author, text))
+		mf = msg_date + datetime.timedelta(hours = UTC_time)  #костыли из-за часовых поясов
+		md = date + datetime.timedelta(hours = UTC_time)      #костыли из-за часовых поясов
+		await ctx.channel.send("Принято: сообщение \"{}\" будет отправлено {}.".format(text, md.strftime("%d.%m.%Y в %H:%M")))
+		self.bot.timer_manager.create_timer("reminder", date, args=(ctx.channel.id, mf.strftime("%d.%m.%Y %H:%M"), ctx.author, text))
 
 
 	@commands.Cog.listener()
