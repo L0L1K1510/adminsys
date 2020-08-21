9ef8c95d283fa2cbf5407daec18e10e212f78d4d
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-21 23:14:18 +0300

    Update Search.py

--- cogs/Search.py
+++ cogs/Search.py
@@ -3,16 +3,16 @@ import asyncio
 from discord.ext import commands
 
 class Search(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+	
+	def __init__(self, bot):
+		self.bot = bot
 
 
-  @commands.command()
+	@commands.command()
 	async def search(self, ctx, *, text):
-    command = text
-    
-    ctx.send('Google: https://google.com/?#q=' + command)
-    ctx.send('Youtube: https://www.youtube.com/results?search_query=' + command)
+		command = text
+		ctx.send('Google: https://google.com/?#q=' + command)
+		ctx.send('Youtube: https://www.youtube.com/results?search_query=' + command)
 
 def setup(bot):
 	bot.add_cog(Search(bot))
