acd58c559921a36f29e6cb6ea207dd148a8c63bf
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-21 23:12:53 +0300

    Update Search.py

--- cogs/Search.py
+++ cogs/Search.py
@@ -1,15 +1,18 @@
-# For Google Search
+import discord
+import asyncio
+from discord.ext import commands
 
-import webbrowser
+class Search(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
 
-command = input('Enter a command')
 
-webbrowser.open('https://google.com/?#q=' + command)
+  @commands.command()
+	async def search(self, ctx, *, text):
+    command = text
+    
+    ctx.send('Google: https://google.com/?#q=' + command)
+    ctx.send('Youtube: https://www.youtube.com/results?search_query=' + command)
 
-# For YouTube Search
-
-import webbrowser
-
-command = input('Enter a command')
-
-webbrowser.open('https://www.youtube.com/results?search_query=' + command)
+def setup(bot):
+	bot.add_cog(Search(bot))
