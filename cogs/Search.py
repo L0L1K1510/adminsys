add35471add3aad6c67dd1e2892dde013e5f35f1
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-21 23:08:05 +0300

    Create Search.py

---
+++ cogs/Search.py
@@ -0,0 +1,15 @@
+# For Google Search
+
+import webbrowser
+
+command = input('Enter a command')
+
+webbrowser.open('https://google.com/?#q=' + command)
+
+# For YouTube Search
+
+import webbrowser
+
+command = input('Enter a command')
+
+webbrowser.open('https://www.youtube.com/results?search_query=' + command)
