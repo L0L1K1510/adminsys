f08a733116ff60b2008c526d4208adca84c703a0
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-22 22:10:39 +0300

    Update Search.py

--- cogs/Search.py
+++ cogs/Search.py
@@ -14,14 +14,12 @@ class Search(commands.Cog):
 
 
 	@commands.command()
-	async def ggsearch(self, ctx, *, text):
-		url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&" + \
-		urllib.parse.urlencode({'q':text});
-		referer = "http://wikipedia.org"
-		h = httplib2.Http({})
-		resp, content = h.request(url, "GET", headers={'Referer': referer})
-		if resp.status == 200: pprint.pprint( json.loads(content) )
-		else: print('Error')
+	async def ytsearch(self, ctx, *, text):
+		url = "https://www.youtube.com/results?" + \
+		urllib.parse.urlencode({'search_query':text});
+		content = urllib.request.urlopen(url)
+		result = re.findall('href=\"\\/watch\\?v=(.{11})', content.read().decode())
+		await ctx.send('https://www.youtube.com/watch?v=' + result[0])
 
 def setup(bot):
 	bot.add_cog(Search(bot))
