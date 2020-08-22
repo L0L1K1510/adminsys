5bb9f007a3a3696f696efacaec7d1902032bf76b
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-22 18:27:15 +0300

    Update Search.py

--- cogs/Search.py
+++ cogs/Search.py
@@ -2,6 +2,11 @@ import discord
 import asyncio
 from discord.ext import commands
 
+import urllib
+import pprint
+import json
+import httplib2
+
 class Search(commands.Cog):
 	
 	def __init__(self, bot):
@@ -9,9 +14,14 @@ class Search(commands.Cog):
 
 
 	@commands.command()
-	async def ytsearch(self, ctx, *, text):
-		command = text.replace(" ", "+")
-		await ctx.send('Youtube: https://www.youtube.com/results?search_query=' + command)
+	async def ggsearch(self, ctx, *, text):
+		url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&" + \
+		urllib.urlencode({'q':query});
+		referer = "http://wikipedia.org"
+		h = httplib2.Http({})
+		resp, content = h.request(url, "GET", headers={'Referer': referer})
+		if resp.status == 200: pprint.pprint( json.loads(content) )
+		else: print('Error')
 
 def setup(bot):
 	bot.add_cog(Search(bot))
