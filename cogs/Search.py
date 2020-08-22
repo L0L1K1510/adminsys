33c69a924d979df8d034ec07c2bcd1c131c458af
L0L1K1510 <parkin.maks@yandex.ru>
2020-08-23 02:42:59 +0300

    Update Search.py

--- cogs/Search.py
+++ cogs/Search.py
@@ -21,7 +21,7 @@ class Search(commands.Cog):
 		f.close()
 		
 		f = open('result.txt', 'r')
-		result = re.findall('videoId\":\"[a-zA-Z0-9]+', f)
+		result = re.findall('videoId\":\"[a-zA-Z0-9]+', f.read())
 		#for res in result:
 			#res = res[9: ]
 		print(result)
